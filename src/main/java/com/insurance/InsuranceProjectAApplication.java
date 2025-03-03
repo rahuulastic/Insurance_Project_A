315cb364b3132edcdfe55e30909f0644364135d6
Shilpa Mali <malishilpa037@gmail.com>
2025-03-03 15:44:14 +0530

    Updated feature branch with new files

---
+++ src/main/java/com/insurance/InsuranceProjectAApplication.java
@@ -0,0 +1,13 @@
+package com.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceProjectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceProjectAApplication.class, args);
+	}
+
+}
